0011da4a6a0a5ce70ca565d66635cc27cfacebd7
Randy Brilliant <randybrilliant68@gmail.com>
2022-10-20 11:19:48 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary' : '#F2E5D2',
+      'secondary' : '#C09551',
+      'tertiary' : '#F6D4B8',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
