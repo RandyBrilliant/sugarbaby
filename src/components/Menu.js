8340a1c7a803e09e8d88f93439ed33fc375602d2
unknown <randybrilliant68@gmail.com>
2022-10-31 20:46:51 +0700

    Added Hero Section

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,63 +6,243 @@ import { PrimaryButton } from './Button';
 
 const Menu = () => {
   return (
-    <section id="menu" className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 place-items-center place-content-center">
-      <div className="grid gap-8 row-gap-5 mb-8 grid-cols-2 lg:grid-cols-3 lg:row-gap-8">
-        <div className="group text-center">
-          <img
-            className="object-cover object-center w-full h-56 mb-6 md:h-64 xl:h-96 group-hover:scale-125 transition duration-300"
-            src={P1}
-            alt="Cake 1"
-          />
-          <p className="mb-1 lg:mb-2 text-lg lg:text-xl font-bold leading-none sm:text-2xl text-secondary">
-            Strawberry Bliss
-          </p>
-          <p className="text-gray-700 font-serif italic">
-            Rp. 30.000,-
-          </p>
+    <section id="menu" className="px-4 sm:px-6 lg:px-20 lg:mt-20 mt-10">
+      <div class="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-6 gap-y-12 w-full mt-6">
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
         </div>
-        <div className="group text-center">
-          <img
-            className="object-cover object-center w-full h-56 mb-6 md:h-64 xl:h-96 group-hover:scale-125 transition duration-300"
-            src={P2}
-            alt="Cake 1"
-          />
-          <p className="mb-1 lg:mb-2 text-lg font-bold leading-none sm:text-2xl text-secondary">
-            Milk Tea Chocolate
-          </p>
-          <p className="text-gray-700 font-serif italic">
-            Rp. 30.000,-
-          </p>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
         </div>
-        <div className="group text-center">
-          <img
-            className="object-cover object-center w-full h-56 mb-6 md:h-64 xl:h-96 group-hover:scale-125 transition duration-300"
-            src={P3}
-            alt="Cake 1"
-          />
-          <p className="mb-1 lg:mb-2 text-lg font-bold leading-none sm:text-2xl text-secondary">
-            Vanilla Creme Brulee
-          </p>
-          <p className="text-gray-700 font-serif italic">
-            Rp. 30.000,-
-          </p>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
         </div>
-        <div className="group text-center">
-          <img
-            className="object-cover object-center w-full h-56 mb-6 md:h-64 xl:h-96 group-hover:scale-125 transition duration-300"
-            src={P4}
-            alt="Cake 1"
-          />
-          <p className="mb-1 lg:mb-2 text-lg font-bold leading-none sm:text-2xl text-secondary">
-            Mango Tango
-          </p>
-          <p className="text-gray-700 font-serif italic">
-            Rp. 30.000,-
-          </p>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
+        </div>
+        <div>
+          <a href="#" class="block h-64 rounded-lg shadow-lg bg-white"></a>
+          <div class="flex items-center justify-between mt-3">
+            <div>
+              <a href="#" class="font-medium">Product Name</a>
+              <a class="flex items-center" href="#">
+                <span class="text-xs font-medium text-gray-600">by</span>
+                <span class="text-xs font-medium ml-1 text-indigo-500">Store Name</span>
+              </a>
+            </div>
+            <span class="flex items-center h-8 bg-indigo-200 text-indigo-600 text-sm px-2 rounded">$34</span>
+          </div>
         </div>
       </div>
-      <div className="flex items-center justify-center">
-        <PrimaryButton href="/" content="Explore More" />
+      <div class="flex justify-center mt-10 space-x-1">
+        <button class="flex items-center justify-center h-8 w-8 rounded text-gray-400">
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+          </svg>
+        </button>
+        <button class="flex items-center justify-center h-8 px-2 rounded text-sm font-medium text-gray-400" disabled>
+          Prev
+        </button>
+        <button class="flex items-center justify-center h-8 w-8 rounded bg-indigo-200 text-sm font-medium text-indigo-600" disabled>
+          1
+        </button>
+        <button class="flex items-center justify-center h-8 w-8 rounded hover:bg-indigo-200 text-sm font-medium text-gray-600 hover:text-indigo-600">
+          2
+        </button>
+        <button class="flex items-center justify-center h-8 w-8 rounded hover:bg-indigo-200 text-sm font-medium text-gray-600 hover:text-indigo-600">
+          3
+        </button>
+        <button class="flex items-center justify-center h-8 px-2 rounded hover:bg-indigo-200 text-sm font-medium text-gray-600 hover:text-indigo-600">
+          Next
+        </button>
+        <button class="flex items-center justify-center h-8 w-8 rounded hover:bg-indigo-200 text-gray-600 hover:text-indigo-600">
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+          </svg>
+        </button>
       </div>
     </section>
   );
